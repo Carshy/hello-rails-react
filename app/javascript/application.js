1b495ad2d50e34094f62113449b798ddfb98e8a2
Collins <mussocollins@gmail.com>
2022-12-01 19:41:56 +0300

    Add rails react using jbundler

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,26 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+import ReactDOM from "react-dom";
+import Greeting from "./Greeting";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/greeting" element={<Greeting />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :greetings
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # get '/greeting', to: 'root#index'
+  root 'root#index'
+  
 end
